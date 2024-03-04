2b21b28d3771953d82bd235c83ef210e933a514a
kalsram <160571164+kalsram@users.noreply.github.com>
2024-03-04 19:29:48 +0530

    Add files via upload

---
+++ function.dart
@@ -0,0 +1,29 @@
+import 'package:firebase/view/home.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+// Future createUserWithEmailAndPassword(mail, pass,context) async {
+//     print(mail);
+//     print(pass);
+
+   
+//       await FirebaseAuth.instance
+//           .createUserWithEmailAndPassword(
+//         email: mail,
+//         password: pass,
+//       )
+//           .then((value) {
+//         Navigator.push(
+//           context,
+//           MaterialPageRoute(builder: (context) => Home()),
+//         );
+//       });
+     
+//     } on FirebaseAuthException catch (e) {
+//       print(e);
+//     } catch (e) {
+      
+//       print(e);
+//     }
+  
